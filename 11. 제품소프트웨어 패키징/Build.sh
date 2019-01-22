2df1ada5f2effe8b91cfada21477ca579efb5992
NaJinWook <43329117+NaJinWook@users.noreply.github.com>
2019-01-22 16:22:04 +0900

    Add files via upload

---
+++ 11. 제품소프트웨어 패키징/Build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+###############################################################
+# Service > systemd > dotnet.service
+# Sample Project > /root
+# ip Change > 192.168.3."You linux ip"
+# MariaDB > user(root)/password(test) > table(test.board)
+###############################################################
+# 1. GitHub Repository Download
+# 2. Project Build
+# 3. Service Shutdown
+# 4. Project Publish
+# 5. Service Run
+###############################################################
+exit 0
